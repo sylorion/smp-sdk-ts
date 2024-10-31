cf7c0762ed64dd863936525b6d504e22b44874ef
J. Charles N. M <jcnm@sylorion.com>
2024-10-31 16:24:38 +0100

    bugfix to be deploy work in local

--- src/service/service.ts
+++ src/service/service.ts
@@ -3,6 +3,34 @@
 import { SMPClient } from './../SMPClient';
 import { serviceQueries } from './../api/graphql/queries/servicesQueries';
 
+/**
+ * Represents a filter input used for querying data.
+ * 
+ * @interface FilterInput
+ * 
+ * @property {string} field - The field to apply the filter on.
+ * @property {string} value - The value to filter by.
+ * @property {string} operator - The operator to use for filtering. 
+ * Can be one of the following: "EQ" (equal), "NE" (not equal), "GT" (greater than), 
+ * "GTE" (greater than or equal), "LT" (less than), "LTE" (less than or equal), 
+ * "IN" (in array), "NIN" (not in array), "LIKE" (like pattern), "NLIKE" (not like pattern).
+ */
+interface FilterInput {
+  field: String, 
+  value: String, 
+  operator: String, // "EQ", "NE", "GT", "GTE", "LT", "LTE", "IN", "NIN", "LIKE", "NLIKE"
+}
+
+interface SortInput  {
+  field: String,
+  order: String, // "ASC" or "DESC"
+}
+
+interface PaginationInput {
+  limit: number,
+  offset: number
+}
+
 export class Service {
   private client: SMPClient;
 
@@ -10,9 +38,8 @@ export class Service {
     this.client = client;
   }
 
-  async list(pagination?: any, sort?: any, filter?: any) {
+  async list(pagination: PaginationInput | undefined = undefined, sort: SortInput | undefined = undefined, filter: FilterInput[] | undefined = []) {
     console.log("Service.list() OK"); 
-
     const query = serviceQueries.GET_SERVICES;
     const variables = {
       pagination,
