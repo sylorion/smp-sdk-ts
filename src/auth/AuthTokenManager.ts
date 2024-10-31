ec1087507aada911d3d68b4c8451c74d902193cf
J. Charles N. M <jcnm@sylorion.com>
2024-10-31 12:36:28 +0100

    bugfix for app login

--- src/auth/AuthTokenManager.ts
+++ src/auth/AuthTokenManager.ts
@@ -53,7 +53,7 @@ export class AuthTokenManager {
 
   public async authenticateApp(appId: string, appSecret: string): Promise<AppLogIn> { 
     try {
-      const appLogin = { appId: appId, appSecret: appSecret };
+      const appLogin = { appId: appId, appKey: appSecret };
       const response = await this.apiClient.query<AppLoginResponse>(MUTATION_AUTH_APP, { appLoginInput: appLogin });
 
       const accessToken = response.authenticateApp.accessToken;
