2eb7bac5e92263182b5e2026b038cfada1818140
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-11-03 10:52:47 +0100

    ajout des mutations de payment

---
+++ src/api/graphql/mutations/accounting/paymentMutations.ts
@@ -0,0 +1,68 @@
+const paymentMutations = {
+    // MUTATION TO CREATE AN INVOICE PAYMENT INTENT
+    CREATE_INVOICE_PAYMENT_INTENT: `
+      mutation CreateInvoicePaymentIntent($input: CreatePaymentIntentInput!) {
+        createInvoicePaymentIntent(input: $input) {
+          clientSecret
+          paymentIntentID
+          message
+        }
+      }
+    `,
+
+    // MUTATION TO PROCESS A PAYMENT
+    PROCESS_PAYMENT: `
+      mutation ProcessPayment($input: ProcessPaymentInput!) {
+        processPayment(input: $input) {
+          status
+        }
+      }
+    `,
+
+    // MUTATION TO REFUND A PAYMENT
+    REFUND_PAYMENT: `
+      mutation RefundPayment($input: RefundPaymentInput!) {
+        refundPayment(input: $input) {
+          status
+          refund {
+            id
+            amount
+            status
+          }
+        }
+      }
+    `,
+
+    // MUTATION TO CREATE AN ESTIMATE FOR PAYMENT
+    CREATE_ESTIMATE_FOR_PAYMENT: `
+      mutation CreateEstimateForPayment($input: CreateEstimateForPaymentInput!) {
+        createEstimateForPayment(input: $input) {
+          estimateID
+          uniqRef
+          slug
+          operatorUserID
+          buyerOrganizationID
+          sellerOrganizationID
+          serviceID
+          expirationDueDate
+          expirationTimeLeft
+          referencePrice
+          previewPrice
+          proposedPrice
+          commentaire
+          negociatedPrice
+          discountID
+          details
+          propositionCount
+          lastProposition
+          stage
+          state
+          createdAt
+          updatedAt
+          deletedAt
+        }
+      }
+    `
+};
+
+export { paymentMutations };
