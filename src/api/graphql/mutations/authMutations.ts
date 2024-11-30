b105460618a6ea802978e243ca354a81e4af136e
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2024-11-30 12:55:24 +0100

    correction de la mutations pour la creation d'utilisateur avant createuser maintenant signup

--- src/api/graphql/mutations/authMutations.ts
+++ src/api/graphql/mutations/authMutations.ts
@@ -98,31 +98,31 @@ export const MUTATION_AUTH_LOGOUT_APP = /* GraphQL */ `
 `;
 
 export const MUTATION_CREATE_USER = /* GraphQL */ `
-  mutation CreateUser($input: CreateUserInput!) {
-    createUser(input: $input) {
-      userID
-      uniqRef
-      slug
-      username
-      email
-      plan
-      profileID
-      userKind
-      lastLogin
-      twoFactorEnabled
-      loginDuration
-      rsaPublicKey
-      state
-      createdAt
-      updatedAt
-      deletedAt
-    }
+  mutation Signup($input: CreateUserInput!) {
+  signup(input: $input) {
+    userID
+    uniqRef
+    slug
+    username
+    email
+    plan
+    profileID
+    userKind
+    lastLogin
+    twoFactorEnabled
+    loginDuration
+    rsaPublicKey
+    state
+    createdAt
+    updatedAt
+    deletedAt
   }
+}
   
 `;
 
 export const MUTATION_FORGOT_PASSWORD = /* GraphQL */ `
- mutation CreateUser($email: String!) {
+ mutation ForgotPassword($email: String!) {
   forgotPassword(email: $email) {
     success
     message
