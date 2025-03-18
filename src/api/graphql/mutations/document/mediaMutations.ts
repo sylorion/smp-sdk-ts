76663b18ba625886de7e51f2655a482c6b1d566b
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2025-03-18 11:56:09 +0100

    ajout de la mutation de creation de media

---
+++ src/api/graphql/mutations/document/mediaMutations.ts
@@ -0,0 +1,24 @@
+const mediaMutations = {
+
+    CREATE_MEDIA: `
+    mutation CreateMedia($input: MediaInput!) {
+        createMedia(input: $input) {
+          mediaID
+          uniqRef
+          slug
+          authorID
+          mediaType
+          legend
+          summary
+          originalName
+          finalName
+          url
+          size
+          state
+          createdAt
+          updatedAt
+          deletedAt
+        }
+      }
+      `
+};
