d51badd7406417d4a7c06b45dfde48758a9e0a2c
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2025-05-16 10:08:15 +0200

    mise à jour des queries des Assets pour qu'elles incluent l'url de la photo de prifle dans le media associé

--- src/api/graphql/queries/user-space/profileQueries.ts
+++ src/api/graphql/queries/user-space/profileQueries.ts
@@ -20,6 +20,9 @@ export const profileQueries = {
           createdAt
           updatedAt
           deletedAt
+          profilePicture {
+            url
+          }
         }
       }
     `,
@@ -45,6 +48,121 @@ export const profileQueries = {
           createdAt
           updatedAt
           deletedAt
+          profilePicture {
+            url
+          }
+        }
+      }
+    `,
+
+    // Query to get a profile by slug
+    GET_PROFILE_BY_SLUG: `
+      query ProfileBySlug($slug: String!) {
+        profileBySlug(Slug: $slug) {
+          profileID
+          uniqRef
+          slug
+          firstName
+          lastName
+          dateOfBirth
+          gender
+          nationality
+          phoneNumber
+          locationID
+          idCardNumber
+          passportNumber
+          socialSecurityNumber
+          state
+          createdAt
+          updatedAt
+          deletedAt
+          profilePicture {
+            url
+          }
+        }
+      }
+    `,
+
+    // Query to get a profile by uniqRef
+    GET_PROFILE_BY_UNIQ_REF: `
+      query ProfileByUniqRef($uniqRef: String!) {
+        profileByUniqRef(uniqRef: $uniqRef) {
+          profileID
+          uniqRef
+          slug
+          firstName
+          lastName
+          dateOfBirth
+          gender
+          nationality
+          phoneNumber
+          locationID
+          idCardNumber
+          passportNumber
+          socialSecurityNumber
+          state
+          createdAt
+          updatedAt
+          deletedAt
+          profilePicture {
+            url
+          }
+        }
+      }
+    `,
+
+    // Query to get profiles by IDs
+    GET_PROFILES_BY_IDS: `
+      query ProfilesByIds($profileIDs: [ID!]!) {
+        profilesByIDs(profileIDs: $profileIDs) {
+          profileID
+          uniqRef
+          slug
+          firstName
+          lastName
+          dateOfBirth
+          gender
+          nationality
+          phoneNumber
+          locationID
+          idCardNumber
+          passportNumber
+          socialSecurityNumber
+          state
+          createdAt
+          updatedAt
+          deletedAt
+          profilePicture {
+            url
+          }
+        }
+      }
+    `,
+
+    // Query to get profiles by slugs
+    GET_PROFILES_BY_SLUGS: `
+      query ProfilesBySlugs($slugs: [String!]!) {
+        profilesBySlugs(slugs: $slugs) {
+          profileID
+          uniqRef
+          slug
+          firstName
+          lastName
+          dateOfBirth
+          gender
+          nationality
+          phoneNumber
+          locationID
+          idCardNumber
+          passportNumber
+          socialSecurityNumber
+          state
+          createdAt
+          updatedAt
+          deletedAt
+          profilePicture {
+            url
+          }
         }
       }
     `,
