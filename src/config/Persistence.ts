53c24b0b7f854ca80ed74305fb0f7111538338a0
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2026-03-26 01:15:32 +0100

    align persistance

--- src/config/Persistence.ts
+++ src/config/Persistence.ts
@@ -1,38 +1,36 @@
 export type PersistenceKind = 'cookie' | 'localStorage' | 'sessionStorage' | 'memory'
 
 export interface PersistenceType {
-  set(key: string, value: any): boolean;
-  get(key: string): string;
-  remove(key: string): boolean;
+    set(key: string, value: any): boolean;
+    get(key: string): string;
+    remove(key: string): boolean;
 }
 
 export class Persistence implements PersistenceType {
     private persistenceKind: PersistenceKind;
-    private memoryStore: Record<string, any>            = {};
-    public static LocalStorageKind:PersistenceKind      = 'localStorage';
-    public static CookieKind:PersistenceKind            = 'cookie';
-    public static SessionStorageKind:PersistenceKind    = 'sessionStorage';
-    public static MemoryKind:PersistenceKind            = 'memory';
-    
+    private memoryStore: Record<string, any> = {};
+    public static LocalStorageKind: PersistenceKind = 'localStorage';
+    public static CookieKind: PersistenceKind = 'cookie';
+    public static SessionStorageKind: PersistenceKind = 'sessionStorage';
+    public static MemoryKind: PersistenceKind = 'memory';
+
     constructor(storageKind: PersistenceKind = 'localStorage') {
-        this.persistenceKind  = storageKind;
+        this.persistenceKind = storageKind;
     }
 
     set(key: string, value: any) {
-        let localStorage = undefined;
-        let sessionStorage = undefined;
-        if (typeof window !== 'undefined' && window.localStorage){
-            localStorage = window.localStorage;
-        }
-        if (typeof window !== 'undefined' && window.sessionStorage){
-            sessionStorage = window.sessionStorage;
-        }
         if (this.persistenceKind === 'cookie') {
-            document.cookie = `${key}=${value}; Secure; HttpOnly;`;
+            if (typeof document !== 'undefined') {
+                document.cookie = `${key}=${value}; Secure; HttpOnly;`;
+            }
         } else if (this.persistenceKind === 'localStorage') {
-            localStorage?.setItem(key, JSON.stringify(value));
+            if (typeof window !== 'undefined' && window.localStorage) {
+                window.localStorage.setItem(key, JSON.stringify(value));
+            }
         } else if (this.persistenceKind === 'sessionStorage') {
-            sessionStorage?.setItem(key, JSON.stringify(value));
+            if (typeof window !== 'undefined' && window.sessionStorage) {
+                window.sessionStorage.setItem(key, JSON.stringify(value));
+            }
         } else {
             this.memoryStore[key] = value;
         }
@@ -40,22 +38,23 @@ export class Persistence implements PersistenceType {
     }
 
     get(key: string): any {
-        let localStorage = undefined;
-        let sessionStorage = undefined;
-        if (typeof window !== 'undefined' && window.localStorage){
-            localStorage = window.localStorage;
-        }
-        if (typeof window !== 'undefined' && window.sessionStorage){
-            sessionStorage = window.sessionStorage;
-        }
         if (this.persistenceKind === 'cookie') {
-            const cookies = document.cookie.split('; ');
-            const cookie = cookies.find((c) => c.startsWith(key));
-            return cookie ? cookie.split('=')[1] : null;
+            if (typeof document !== 'undefined') {
+                const cookies = document.cookie.split('; ');
+                const cookie = cookies.find((c) => c.startsWith(key));
+                return cookie ? cookie.split('=')[1] : null;
+            }
+            return null;
         } else if (this.persistenceKind === 'localStorage') {
-            return JSON.parse(localStorage?.getItem(key) || 'null');
+            if (typeof window !== 'undefined' && window.localStorage) {
+                return JSON.parse(window.localStorage.getItem(key) || 'null');
+            }
+            return null;
         } else if (this.persistenceKind === 'sessionStorage') {
-            return JSON.parse(sessionStorage?.getItem(key) || 'null');
+            if (typeof window !== 'undefined' && window.sessionStorage) {
+                return JSON.parse(window.sessionStorage.getItem(key) || 'null');
+            }
+            return null;
         } else {
             return this.memoryStore[key] || null;
         }
@@ -63,11 +62,17 @@ export class Persistence implements PersistenceType {
 
     remove(key: string) {
         if (this.persistenceKind === 'cookie') {
-            document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
+            if (typeof document !== 'undefined') {
+                document.cookie = `${key}=; expires=Thu, 01 Jan 1970 00:00:00 GMT;`;
+            }
         } else if (this.persistenceKind === 'localStorage') {
-            localStorage.removeItem(key);
+            if (typeof window !== 'undefined' && window.localStorage) {
+                window.localStorage.removeItem(key);
+            }
         } else if (this.persistenceKind === 'sessionStorage') {
-            sessionStorage.removeItem(key);
+            if (typeof window !== 'undefined' && window.sessionStorage) {
+                window.sessionStorage.removeItem(key);
+            }
         } else {
             delete this.memoryStore[key];
         }
