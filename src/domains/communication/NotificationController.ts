2bb561446692e9ecbad03622e262de714836e79b
zaengetsu <106625404+zaengetsu@users.noreply.github.com>
2026-04-07 16:29:54 +0200

    correction de la recuperationd es notification

--- src/domains/communication/NotificationController.ts
+++ src/domains/communication/NotificationController.ts
@@ -16,63 +16,63 @@ export class Notification {
   async list(pagination?: any, sort?: any, filter?: any): Promise<any[]> {
     const query = notificationQueries.GET_NOTIFICATIONS;
     const variables = { pagination, sort, filter };
-    const response = await this.client.query(query, variables) as { data: { notifications: any[] } };
-    return response.data.notifications;
+    const response = await this.client.query(query, variables) as { notifications: any[] };
+    return response.notifications;
   }
 
   async getById(notificationID: string): Promise<any> {
     const query = notificationQueries.GET_NOTIFICATION_BY_ID;
     const variables = { notificationID };
-    const response = await this.client.query(query, variables) as { data: { notificationByID: any } };
-    return response.data.notificationByID;
+    const response = await this.client.query(query, variables) as { notificationByID: any };
+    return response.notificationByID;
   }
 
   async getByIds(notificationIDs: string[]): Promise<any[]> {
     const query = notificationQueries.GET_NOTIFICATIONS_BY_IDS;
     const variables = { notificationIDs };
-    const response = await this.client.query(query, variables) as { data: { notificationsByIDs: any[] } };
-    return response.data.notificationsByIDs;
+    const response = await this.client.query(query, variables) as { notificationsByIDs: any[] };
+    return response.notificationsByIDs;
   }
 
   async getByUniqRef(uniqRef: string): Promise<any> {
     const query = notificationQueries.GET_NOTIFICATION_BY_UNIQ_REF;
     const variables = { uniqRef };
-    const response = await this.client.query(query, variables) as { data: { notificationByUniqRef: any } };
-    return response.data.notificationByUniqRef;
+    const response = await this.client.query(query, variables) as { notificationByUniqRef: any };
+    return response.notificationByUniqRef;
   }
 
   async getBySlug(slug: string): Promise<any> {
     const query = notificationQueries.GET_NOTIFICATION_BY_SLUG;
     const variables = { slug };
-    const response = await this.client.query(query, variables) as { data: { notificationBySlug: any } };
-    return response.data.notificationBySlug;
+    const response = await this.client.query(query, variables) as { notificationBySlug: any };
+    return response.notificationBySlug;
   }
 
   async getBySlugs(slugs: string[]): Promise<any[]> {
     const query = notificationQueries.GET_NOTIFICATIONS_BY_SLUGS;
     const variables = { slugs };
-    const response = await this.client.query(query, variables) as { data: { notificationsBySlugs: any[] } };
-    return response.data.notificationsBySlugs;
+    const response = await this.client.query(query, variables) as { notificationsBySlugs: any[] };
+    return response.notificationsBySlugs;
   }
 
   async getByUserId(userID: string): Promise<any[]> {
     const query = notificationQueries.GET_NOTIFICATIONS_BY_USER_ID;
     const variables = { userID };
-    const response = await this.client.query(query, variables) as { data: { notificationsByUserID: any[] } };
-    return response.data.notificationsByUserID;
+    const response = await this.client.query(query, variables) as { notificationsByUserID: any[] };
+    return response.notificationsByUserID;
   }
 
   async getByOrganizationId(organizationID: string): Promise<any[]> {
     const query = notificationQueries.GET_NOTIFICATIONS_BY_ORGANIZATION_ID;
     const variables = { organizationID };
-    const response = await this.client.query(query, variables) as { data: { notificationsByOrganizationID: any[] } };
-    return response.data.notificationsByOrganizationID;
+    const response = await this.client.query(query, variables) as { notificationsByOrganizationID: any[] };
+    return response.notificationsByOrganizationID;
   }
 
   async markAsRead(notificationID: string): Promise<any> {
     const query = notificationQueries.MARK_NOTIFICATION_AS_READ;
     const variables = { notificationID };
-    const response = await this.client.mutate(query, variables) as { data: { markNotificationAsRead: any } };
-    return response.data.markNotificationAsRead;
+    const response = await this.client.mutate(query, variables) as { markNotificationAsRead: any };
+    return response.markNotificationAsRead;
   }
 }
